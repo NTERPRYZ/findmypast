3f88af03e7ab20b36128fb9fe64733d13437b87a
Robert Henderson <robert@Roberts-MacBook-Pro.local>
2019-07-15 17:20:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="logo" src={logo} alt="findmypast-logo" />
       </header>
+      <div className="App-body">
+        <h1 className="center">Prime Number Table Generator</h1>
+        <div className="inputDiv">
+          <h2>Enter Number</h2>
+          <textarea></textarea>
+          <button>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
